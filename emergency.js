add8389821b8c04fe35b599cbf95cccbdc1b21c6
Ezhilarasi <ezhilarasi371@gmail.com>
2026-07-20 22:16:30 +0530

    Added Emergency Blood Request feature

---
+++ emergency.js
@@ -0,0 +1,42 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const form = document.getElementById("bloodRequestForm");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const patientName = document.getElementById("patientName").value;
+    const bloodGroup = document.getElementById("bloodGroup").value;
+    const hospital = document.getElementById("hospital").value;
+    const location = document.getElementById("location").value;
+
+    try {
+
+        await addDoc(collection(db, "EmergencyRequests"), {
+
+            patientName,
+            bloodGroup,
+            hospital,
+            location,
+            status: "Pending",
+            createdAt: serverTimestamp()
+
+        });
+
+        alert("Emergency Blood Request Sent Successfully!");
+
+        form.reset();
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+});
