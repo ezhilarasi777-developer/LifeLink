da58719aaa9806f0b5c009676721a2977ec9a34c
Ezhilarasi <ezhilarasi371@gmail.com>
2026-07-22 21:44:05 +0530

    Added search feature

--- aiPrediction.js
+++ aiPrediction.js
@@ -1,6 +1,11 @@
 // aiPrediction.js
 
-import { db, collection, getDocs } from "./firebase.js";
+import { db } from "./firebase.js";
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 async function predictBloodAvailability(bloodGroup) {
     const snapshot = await getDocs(collection(db, "donors"));
 
